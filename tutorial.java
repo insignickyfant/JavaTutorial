4bc9e9c913891794f29ab4f8e929ce97a6f2bfe1
NickyShears <nwshears@gmail.com>
2021-08-07 20:06:28 +0200

    update

--- tutorial.java
+++ tutorial.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class tutorial {
+    // method: identifier = main, with (parameter(s)). 
+    // Parameter has arguments (the data passed into method/input)
+    // access modifier = public, no object needed = static, 
+    // return type = void (none)
     public static void main(String[] args) { 
-        System.out.println("Hello, World!");
+        // print Hello World!
+        System.out.println("Hello World!");   // output
+        // declare new variable of data type Scanner called 'in' (arbitrary)
+        Scanner in = new Scanner(System.in); // input from console
+        String s = in.nextLine(); // store input in variable s
+        System.out.println(s); // print user input
+
     }
-    
 }
