2c57bbc28f4db97fca35c7e8c1eeb64315e0cd05
NickyShears <nwshears@gmail.com>
2021-08-08 11:28:23 +0200

    mini

--- tutorial.java
+++ tutorial.java
@@ -19,12 +19,13 @@ public class tutorial {
         
         // Day 2: take user input
         //System.out.println("Hello " + args[0]); // direct commandline input arg
-        System.out.println("What's your name bruh?");
+        System.out.println("What's your name bruh?"); // ask user for input first
         // declare new variable of data type Scanner called 'in' (arbitrary)
         Scanner scanner = new Scanner(System.in); // input from console
         String name = scanner.nextLine(); // store input in variable s
         System.out.println("Hi " + name + "!"); // print user input s
 
+        // Day 3
 
     }
 }
